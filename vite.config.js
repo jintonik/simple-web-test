7916a1fffc291f77ed15815786818c1ac12d4191
Anton <asherpaev@yandex.ru>
2025-11-18 20:40:12 +0700

    vite.config fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: 'simple-web-app',
   // Отключаем точку входа — работаем с index.html напрямую
   appType: 'mpa', // Multi-Page Application — для статики
   build: {
